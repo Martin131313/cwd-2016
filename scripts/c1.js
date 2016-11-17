a6604959b6b40779ec5fd477ce8430772711a7c8
Martin131313 <moc@semosedu.local>
2016-11-17 12:35:17 +0100

    update

---
+++ scripts/c1.js
@@ -0,0 +1,146 @@
+// var ime = 'Bojan'; // string
+// var vozrast =  34; // number(integer)
+// var visina = 182.4; // number(float)
+// var vozachka_dozvola = true; // boolean
+
+// //vozachka_dozvola - snake case
+// //vozachkaDozvola - camel case
+
+// console.log('Zdravo Svetu!');
+
+// console.log(ime);
+// console.log(vozrast);
+// console.log(visina);
+// console.log(vozachka_dozvola);
+
+// console.debug('Debugging some variables...');
+// console.error('My name is not ' + ime);
+// console.info('This guy is this high:' + visina + 'cm');
+
+
+
+// var vozilo = 'tochak';
+// var model = 'poni';
+// var trkala = 'dve';
+// var brzini = ('ima');
+// var kochnica = 'kontra';
+// var sedishteplastichno = true;
+// var boja = 'plava';
+// var svetlo ='edno';
+// var gidan ='ima';
+// var koleminanabandash = 16;
+// var kradeno = true;
+// var amortizeri = false;
+
+
+
+// console.log('Prevoz');
+// console.log(vozilo);
+// console.log('model');
+// console.log('trkala');
+// console.log('brzini');
+// console.log('kochnica');
+// console.info(sedishteplastichno);
+// console.log('boja');
+// console.log('svetlo');
+// console.log('ima');
+// console.log('goleminanabandash');
+// console.info(kradeno);
+// console.info(amortizeri);
+
+
+// var a =10;
+// var b =13;
+// var c =4;
+// var d=22;
+
+// var e = a + b * c / d ;
+// console.log(e);
+
+// var f = (a + b) * (c / b);
+// console.log(f);
+
+
+
+// var g = 20;
+// var h = 3;
+
+// console.log(g % h);
+
+
+
+
+
+
+
+// function zdravo(){
+// 	console.log('Zdravo Svetu');
+// }
+
+
+// zdravo();
+
+
+
+
+
+// function pozdrav(ime){
+// 	console.log('Zdravo' + ime);
+// }
+
+// pozdrav('Pero');
+// pozdrav('Janko');
+// pozdrav('Stanko');
+
+// function f2c(f){
+// 	console.log((f=60 - 32) / 1.8);
+// }
+
+// f2c(60)
+
+
+
+// function c2f(c){
+// 	console.log(c=40 * 1.8 + 32);
+// }
+
+// c2f(40)
+
+
+
+
+function f2c(far){
+	var output = (far - 32) / 1.8;
+	console.log(output);
+}
+
+function c2f(cel){
+	var output = cel * 1.8 + 32;
+	console.log(output);
+}
+
+
+f2c(100);
+
+c2f(38);
+
+
+
+/*
+
+ inches to centimeters i2c
+ centimeters to inches c2i
+
+ pounds to kilograms p2k
+ kilograms to pounds k2p
+
+ miles to kilometers m2k
+ kilometers to miles k2m
+
+ liters to galons l2g
+ galons to liters g2l
+
+ byte to megabyte b2m
+ megabyte to byte m2b
+
+*/
